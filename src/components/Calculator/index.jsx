4b25020cb0abae1328a902e5d78c3395029937a7
mimirdevapp <mimir.dev.app@gmail.com>
2024-10-05 01:07:59 +0530

    added deleted functionality to lists

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -8,6 +8,8 @@ import Add from '@mui/icons-material/Add';
 import Select from '@mui/joy/Select';
 import Option from '@mui/joy/Option';
 import Input from '@mui/joy/Input';
+import Alert from '@mui/material/Alert';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import './index.css';
 
 const Calculator = () => {
@@ -32,6 +34,7 @@ const Calculator = () => {
   });
   const [totalExpense, setTotalExpense] = useState(0);
   const [result, setResult] = useState(new Map());
+  const [isErrorOccurred, setIsErrorOccurred] = useState(false);
 
   function convertStringNumbersToInt(obj) {
     const newObj = {...obj}
@@ -68,7 +71,6 @@ const Calculator = () => {
     };
 
     const input = convertStringNumbersToInt(stringInput);
-    console.log(input);
     
     if(checkEqual(input)) {
       const friendsMap = new Map();
@@ -87,17 +89,29 @@ const Calculator = () => {
         friendsMap.set(friend, (cost + commonCost).toFixed(2))
       });
       setResult(friendsMap);
+      setIsErrorOccurred(false);
     } else {
-      console.log("Total Cost is not equal to expenses!");
+      setIsErrorOccurred(true);
     }
   }
 
-  const addFriend = () => {
+  const addFriend = (e) => {
+    e.preventDefault();
     if (friendsInput.trim() !== '') {
       setFriends([...friends, friendsInput]);
       setFriendsInput('');
     };  
   };
+
+  const removeFriend = (index) => {
+    const updatedFriends = friends.filter((val, friendIndex) => friendIndex != index);
+    setFriends(updatedFriends);
+  }
+
+  const removeExpense = (index) => {
+    const updatedExpenses = expenses.filter((val, expenseIndex) => expenseIndex != index);
+    setExpenses(updatedExpenses);
+  }
 
   const addExpense = () => {
     if (expenseInput.expense.trim() !== '' && expenseInput.cost !== '' && expenseInput.friends.length !== 0) {
@@ -131,7 +145,7 @@ const Calculator = () => {
       <br />
 
       <div className="friends">
-        <div className="friends-input">
+        <form className="friends-input" onSubmit={(e) => addFriend(e)}>
           <Input
             value={friendsInput}
             onChange={(e) => setFriendsInput(e.target.value)}
@@ -139,17 +153,22 @@ const Calculator = () => {
             sx={{ width: 375 }}
           />
           <br />
-          <IconButton 
-            onClick={addFriend} 
+          <IconButton
             variant="solid"
+            type="submit"
             style={{ margin: '0 0.5rem ' }}
             color='primary'>
             <PersonAddIcon />
           </IconButton>
-        </div>
+        </form>
         <ul>
           {friends.map((friend, index) => (
-            <li key={index}>{friend}</li>
+            <div>
+              <li className='friends-list' key={index}>
+                <span className='friends-list-name'>{friend}</span>
+                <HighlightOffIcon onClick={() => removeFriend(index)} className='delete-button float-button' fontSize="small"/>
+              </li>
+            </div>
           ))}
         </ul>
       </div>
@@ -157,39 +176,39 @@ const Calculator = () => {
       <div className="expenses">
         <h2>Expenses</h2>
         <h4>Add item and its corresponding cost</h4>
-        <Input
-          placeholder="Expense"
-          value={expenseInput.expense}
-          onChange={(e) => setExpenseInput(({...expenseInput, expense: e.target.value}))}
-          sx={{ width: 375 }}
-        />
-        <br />
-        <Input
-          placeholder="Cost"
-          startDecorator={'₹'}
-          value={expenseInput.cost}
-          onChange={(e) => setExpenseInput(({...expenseInput, cost: e.target.value}))}
-          sx={{ width: 375 }}
-        />
-        <br />
+          <Input
+            placeholder="Expense"
+            value={expenseInput.expense}
+            onChange={(e) => setExpenseInput(({...expenseInput, expense: e.target.value}))}
+            sx={{ width: 375 }}
+          />
+          <br />
+          <Input
+            placeholder="Cost"
+            startDecorator={'₹'}
+            value={expenseInput.cost}
+            onChange={(e) => setExpenseInput(({...expenseInput, cost: e.target.value}))}
+            sx={{ width: 375 }}
+          />
+          <br />
 
-        {friends.length !== 0 ?
-          <Select onChange={handleFriendSelect} placeholder="Select Friend" multiple sx={{ width: 375 }}>
-            {friends.map((friend) => (
-              <Option key={friend} value={friend}>{friend}</Option>
-            ))}
-          </Select> : <Select placeholder="Select friends" disabled sx={{ width: 375 }} />
-        }
-        <br />
-        <Button
-          onClick={addExpense} 
-          variant="solid"
-          startDecorator={<Add/>}>
-          ADD EXPENSE
-        </Button>
+          {friends.length !== 0 ?
+            <Select onChange={handleFriendSelect} placeholder="Select Friend" multiple sx={{ width: 375 }}>
+              {friends.map((friend) => (
+                <Option key={friend} value={friend}>{friend}</Option>
+              ))}
+            </Select> : <Select placeholder="Select friends" disabled sx={{ width: 375 }} />
+          }
+          <br />
+          <Button
+            onClick={addExpense} 
+            variant="solid"
+            startDecorator={<Add/>}>
+            ADD EXPENSE
+          </Button>
         <ul>
           {expenses.map((expense, index) => (
-            <li key={index}>{expense.expense}  -  Rs. {expense.cost}  -  {expense.friends.join(", ")}</li>
+            <li className="expenses-list" key={index}><span className="expenses-list-name">{expense.expense}  -  Rs. {expense.cost}  -  {expense.friends.join(", ")}</span><HighlightOffIcon onClick={() => removeExpense(index)} className='delete-button' fontSize="small"/></li>
           ))}
         </ul>
       </div>
@@ -278,11 +297,17 @@ const Calculator = () => {
         endDecorator={<KeyboardArrowRightIcon  />}>
         CALCULATE
       </Button>
-      <ul>
-        {Array.from(result).map(([name, value]) => (
-          <li key={name}>{name} - {value}</li>
-        ))}
-      </ul>
+      {!isErrorOccurred ? 
+        <ul>
+          {Array.from(result).map(([name, value]) => (
+            <li key={name}>{name} - {value}</li>
+          ))}
+        </ul> 
+        :
+        <div className='error-alert'>
+          <Alert severity="error">Total cost is not adding up to expenses!</Alert>
+        </div>
+      }
     </div>
   );
 }
